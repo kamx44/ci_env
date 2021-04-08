ac8874116edabd93fd7ca700b5f1803d1952612f
KJ <kamx44@gmail.com>
2021-04-08 20:23:56 +0000

    added artifactory dockerfile and building and cleaning artifactory

--- clean.sh
+++ clean.sh
@@ -2,3 +2,5 @@
 
 docker rm -f jenkins
 docker rmi jenkins:ci_env
+docker rm -f artifactory
+docker rmi artifactory:ci_env
--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#Start Jenkins
+#---Start Jenkins---
+#check if volume exist if not create a new one
+docker volume inspect jenkins_volume || docker volume create jenkins_volume;
+#build and run jenkins
 docker build -t jenkins:ci_env ./dockerfiles/jenkins
 docker run --name jenkins -d -p 8880:8080 -p 50000:50000 -v jenkins_volume:/var/jenkins_home jenkins:ci_env
 
-#Start Artifactory
-#docker build -t artifactory:ci_env ./dockerfiles/artifactory
+#---Start Artifactory---
+#check if volume exist if not create a new one
+docker volume inspect artifactory_volume || docker volume create artifactory_volume;
+#build and run jenkins
+docker build -t artifactory:ci_env ./dockerfiles/artifactory
+docker run --name artifactory -d -p 8081:8081 -p 8082:8082 -v artifactory_volume:/var/opt/jfrog/artifactory artifactory:ci_env
