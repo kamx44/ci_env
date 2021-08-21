30d075f6c8f316f4d88189f7deb76537999d8aa7
Kamil Jakubik <kamx44@gmail.com>
2021-08-21 11:00:23 +0000

    added ansible playbook to deploy docker

--- start.sh
+++ start.sh
@@ -11,20 +11,20 @@ docker run --name jenkins -d -p 8880:8080 -p 50000:50000 --net docker_network -v
 
 #---Start Artifactory---
 #check if volume exist if not create a new one
-docker volume inspect artifactory_volume || docker volume create artifactory_volume;
+#docker volume inspect artifactory_volume || docker volume create artifactory_volume;
 #build and run jenkins
-docker build -t artifactory:ci_env ./dockerfiles/artifactory
-docker run --name artifactory -d -p 8081:8081 -p 8082:8082 --net docker_network -v artifactory_volume:/var/opt/jfrog/artifactory artifactory:ci_env
+#docker build -t artifactory:ci_env ./dockerfiles/artifactory
+#docker run --name artifactory -d -p 8081:8081 -p 8082:8082 --net docker_network -v artifactory_volume:/var/opt/jfrog/artifactory artifactory:ci_env
 
 #--Start Selenium HUB
-docker build -t selenium-hub:ci_env ./dockerfiles/selenium/hub
-docker run --name selenium-hub -d -p 4444:4444 --net docker_network selenium-hub:ci_env
+#docker build -t selenium-hub:ci_env ./dockerfiles/selenium/hub
+#docker run --name selenium-hub -d -p 4444:4444 --net docker_network selenium-hub:ci_env
 
 #--Start Selenium Node
-docker build -t selenium-node:ci_env ./dockerfiles/selenium/node
-docker run --name selenium-node -d --net docker_network selenium-node:ci_env
+#docker build -t selenium-node:ci_env ./dockerfiles/selenium/node
+#docker run --name selenium-node -d --net docker_network selenium-node:ci_env
 
 
 #--Start slave server
-docker build -t ubuntuslave:ci_env ./dockerfiles/slave
-docker run --name slave -t -d --net docker_network -v /var/run/docker.sock:/var/run/docker.sock -p 8088:8088 ubuntuslave:ci_env
+#docker build -t ubuntuslave:ci_env ./dockerfiles/slave
+#docker run --name slave -t -d --net docker_network -v /var/run/docker.sock:/var/run/docker.sock -p 8088:8088 ubuntuslave:ci_env
